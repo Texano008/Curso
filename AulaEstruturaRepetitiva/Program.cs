5d8714dd85fa58cfc1ddb7d3ac6e50b1de31cd72
Texano008 <luisrbbrasil098@gmail.com>
2026-02-24 18:29:56 -0300

    Aula Estrutura Repetitiva

---
+++ AulaEstruturaRepetitiva/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AulaEstruturaRepetitiva
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("quantos numeros inteiros voce vai digitar? ");
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++) { 
+                Console.WriteLine("oi");
+            }
+        }
+    }
+}
